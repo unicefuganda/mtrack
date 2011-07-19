5372db2e38fb57ffd0473e0ba7f536ed746f0487
Rowena <rluk@dimagi.com>
2011-07-19 21:54:24 +0300

    add a settings file for the builds erver

---
+++ mtrack_project/settings_test.py
@@ -0,0 +1,11 @@
+# for postgresql:
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "mtrack",
+        "USER": "postgres",
+        "PASSWORD": "test",
+        "HOST": "localhost",
+    }
+}
+

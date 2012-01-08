e845645f5a176a7da6453e8776a702f4748cb4a7
Rowena <rluk@dimagi.com>
2012-01-07 23:14:01 -0500

    usr & update settings

--- mtrack_project/init.sh
+++ mtrack_project/init.sh
@@ -4,4 +4,6 @@
 ./manage.py loaddata healthmodels
 ./manage.py mtrack_init
 ./manage.py check_script_progress -e 7 -l 20
+./manage.py mtrack_verify
+./manage.py mtrack_create_regions static/uganda/District\,\ Sub\ Region\,\ Region.csv
 

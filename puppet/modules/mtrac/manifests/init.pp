0ed4fe0314412066f4a6860d2cc2fc32931acbaf
Chris Ford <christophertford@gmail.com>
2012-12-20 19:14:56 +0300

    Install dependencies and get to the point of migrating.

--- puppet/modules/mtrac/manifests/init.pp
+++ puppet/modules/mtrac/manifests/init.pp
@@ -7,21 +7,7 @@ class mtrac {
     ensure => present,
   }
 
-  # exec { "pip-modules":
-  #   require => [Class["postgres"], Package["python-pip"], Package["python-dev"]],
-  #   command => "/usr/bin/pip install -r /vagrant/pip-requires.txt",
-  #   timeout => 0,
-  # }
-  
-  # exec { "restoredb":
-  #     require => [Class["postgres"], Exec["pip-modules"]],
-  #     command => "/usr/bin/sudo -u postgres -c \"/usr/bin/pg_restore -d $dbname /vagrant/$dumpfile\"",
-  #     timeout => 0,
-  #     returns => [0, 1],
-  #   }
-  # 
-  #   exec { "runserver":
-  #     require => Exec["restoredb"],
-  #     command => "/vagrant/mtrack_project/manage.py runserver &",
-  #   }
+  package { "python-lxml":
+    ensure => present,
+  }
 }

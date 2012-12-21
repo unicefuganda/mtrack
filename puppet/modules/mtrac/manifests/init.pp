abffb7ebe26ef992ffaf1c74c22ef5c76b407b28
Chris Ford <christophertford@gmail.com>
2012-12-21 10:22:43 +0300

    Install binary dependencies for python packages.

--- puppet/modules/mtrac/manifests/init.pp
+++ puppet/modules/mtrac/manifests/init.pp
@@ -1,4 +1,10 @@
 class mtrac {
+
+  exec { "update"
+    command => "apt-get update",
+    path => "/usr/bin/"
+  }
+
   package { "python-pip":
     ensure => present,
   }
@@ -7,8 +13,18 @@ class mtrac {
     ensure => present,
   }
 
+  package { "libxml2-dev":
+    require => Command['update'],
+    ensure => present,
+  }
+
+  package { "libxslt-dev":
+    require => Command['update'],
+    ensure => present,
+  }
 
   package { "python-lxml":
+    require => Package['libxslt-dev', 'libxml2-dev'],
     ensure => present,
   }
 }

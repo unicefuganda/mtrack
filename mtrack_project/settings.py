a01e6a185d13df6ccacf0af09ee083280c63e560
Sri Prasanna <k.sriprasanna@gmail.com>
2013-01-24 10:46:52 +0300

    +Nishant - added django messages to settings

--- mtrack_project/settings.py
+++ mtrack_project/settings.py
@@ -140,6 +140,7 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sites",
     "django.contrib.humanize",
+    "django.contrib.messages",
 
     "eav",
     "healthmodels",

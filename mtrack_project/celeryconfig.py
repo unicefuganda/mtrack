962b386a7e5ae8364b209e2ab3219d495ab69f64
NavaL <mnandri@thoughtworks.com>
2013-04-19 18:16:47 +0300

    disabling hmis033b submission schedule -- for now

--- mtrack_project/celeryconfig.py
+++ mtrack_project/celeryconfig.py
@@ -15,11 +15,11 @@ CELERY_TIME_TO_WAIT_BEFORE_RETRYING_SUBMISSION = 600 # wait 10 mins for supervis
 CELERY_NUMBER_OF_RETRIES_IN_CASE_OF_FAILURE = 1 # only retry once on the same day... the following day submission will pick up the rest.
 
 CELERYBEAT_SCHEDULE = {
-    'submit-reports-everyday_at_10PM': {
-            'task': 'dhis2.reports_submission_tasks.weekly_report_submissions_task',
-            'schedule': crontab(hour=21, minute=59, day_of_week='*'),
-            'args': ()
-        },
+    # 'submit-reports-everyday_at_10PM': {
+    #         'task': 'dhis2.reports_submission_tasks.weekly_report_submissions_task',
+    #         'schedule': crontab(hour=21, minute=59, day_of_week='*'),
+    #         'args': ()
+    #     },
     # 'run-fred-sync-everyday_at_9PM': {
     #         'task': 'fred_consumer.tasks.run_fred_sync',
     #         'schedule': crontab(hour=20, minute=59, day_of_week='*'),
